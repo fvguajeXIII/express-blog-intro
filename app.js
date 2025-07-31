63ee938867ed9d529663955a5000d24b11a0690f
fvguajexiii <f.vellucci13@outlook.it>
2025-07-31 14:23:56 +0200

    Array di post del blog

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
 });
+
+//Array di post del blog
+const post = [
+  {
+    titolo: "Introduzione a Javascript",
+    contenuto: "JavaScript è un linguaggio di programmazione versatile e potente, utilizzato principalmente per lo sviluppo web. In questo post esploreremo le basi del linguaggio e le sue caratteristiche principali.",
+    immagine: "/asset/img/nodejs.png"
+    tags: ["javascript", "programmazione", "web development", "tutorial"]
+  },
+]
